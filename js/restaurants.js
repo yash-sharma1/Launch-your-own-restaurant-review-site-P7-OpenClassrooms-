e057dd19373ddbb900de98db02abe98674d09768
Yash Sharma <44340561+yash-sharma1@users.noreply.github.com>
2020-09-04 17:07:11 +0530

    JS files added

---
+++ js/restaurants.js
@@ -0,0 +1,125 @@
+eqfeed_callback({
+
+    "html_attributions": [],
+    "results": [
+        {
+            "geometry": {
+                "location": {
+                    "lat": 26.808661,
+                    "lng": 75.829887
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+            "id": 0,
+            "name": "Food Fix Restaurant",
+            "formatted_phone_number": "+91-98456321",
+            "url": "foodfix.com",
+            "website": "foodfix.com",
+            "opening_hours": {
+                "open_now": true
+            },
+            "rating": '4',
+            "reviews": [{
+                "name": "Manohar Lal",
+                "rating":5,
+                "text": "Besy tasty and healthy food",
+                "relative_time_description": "2 days ago"
+            }],
+
+            "place_id": "0",
+            "scope": "GOOGLE",
+            "types": ["restaurant"],
+            "vicinity": "Sector 17, Pratap Nagar, Jaipur, Rajasthan 302033"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 26.804062,
+                    "lng": 75.833719
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+            "id": 1,
+            "name": "Tasty Food Corner",
+            "formatted_phone_number": "+91-7836954521",
+            "url": "tastyfoodcorner.com",
+            "website": "tastyfoodcorner.com",
+            "opening_hours": {
+                "open_now": true
+            },
+            "rating": 4,
+            "reviews": [{
+                "name": "David Smith",
+                "rating":4,
+                "text": "not bad food, good music",
+                "relative_time_description": "3 days ago"
+            }],
+
+            "place_id": "1",
+            "scope": "GOOGLE",
+            "types": ["restaurant"],
+            "vicinity": "Janaki Devi School Rd, Sector 19, Pratap Nagar, Jaipur, Rajasthan 302033"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 26.805788,
+                    "lng": 75.822977
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+            "id": 2,
+            "name": "OTC Kebbab and Curry",
+            "formatted_phone_number": "+91-896548932",
+            "url": "otc.com",
+            "website": "otc.com",
+            "opening_hours": {
+                "open_now": true
+            },
+            "rating": 5,
+            "reviews": [{
+                "name": "Yash Shrma",
+                "rating":2,
+                "text": "Mouth Watering Food",
+                "relative_time_description": "10 days ago"
+            }],
+
+            "place_id": "2",
+            "scope": "GOOGLE",
+            "reference": "CoQBeQAAAFvf12y8veSQMdIMmAXQmus1zqkgKQ-O2KEX0Kr47rIRTy6HNsyosVl0CjvEBulIu_cujrSOgICdcxNioFDHtAxXBhqeR-8xXtm52Bp0lVwnO3LzLFY3jeo8WrsyIwNE1kQlGuWA4xklpOknHJuRXSQJVheRlYijOHSgsBQ35mOcEhC5IpbpqCMe82yR136087wZGhSziPEbooYkHLn9e5njOTuBprcfVw",
+            "types": ["restaurant"],
+            "vicinity": "Savitri Vihar, Sanganer, Sector 9, Pratap Nagar, Jaipur, Rajasthan 302033"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 26.807110,
+                    "lng": 75.835680
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+            "id": 3,
+            "name": "Marwadi Dhaba",
+            "formatted_phone_number": "+91-9578627269",
+            "url": "marwadidhaba.com",
+            "website": "marwadidhaba.com",
+            "opening_hours": {
+                "open_now": true
+            },
+            "rating": 4.6,
+            "reviews": [{
+                "name": "Neha",
+               "rating":4,
+               "text": "Best Indian Desi Food Ever",
+               "relative_time_description": "5 days ago"
+            }],
+
+            "place_id": "3",
+            "scope": "GOOGLE",
+            "types": ["restaurant"],
+            "vicinity": "NRI Rd, Sector 17, Pratap Nagar, Jaipur, Rajasthan 302033"
+        }
+    ],
+    "status": "OK"
+
+});
